628f7990a736650ca4945a868ebea9f46d0dac71
super-mario-ai <kazamario@gmail.com>
2024-12-16 06:27:32 +0000

    *

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "https://transport.lv/",
   match: "https://transport.lv/**",
-  maxPagesToCrawl: 99999999,
+  maxPagesToCrawl: 9999999,
   outputFileName: "transport.json",
   maxTokens: 2000000,
   selector: "body",
