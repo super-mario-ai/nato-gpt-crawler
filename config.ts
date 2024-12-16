a28c6dfee567c8e49077d18aacd76ece025b95c4
super-mario-ai <kazamario@gmail.com>
2024-12-16 11:38:57 +0000

    *

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
   url: "https://www.nato.int/",
-  match: "https://www.nato.int//**",
+  match: "https://www.nato.int/**",
   maxPagesToCrawl: 99999999,
   outputFileName: "nato.json",
   maxTokens: 2000000,
