c830bda5f3ad5faab07a01ebd0cad2caf2b1075f
super-mario-ai <kazamario@gmail.com>
2024-12-16 05:46:25 +0000

    *

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "https://vm.ee/en",
   match: "https://vm.ee/en/**",
-  maxPagesToCrawl: 99999999,
+  maxPagesToCrawl: 9999999,
   outputFileName: "vm-ee.json",
   maxTokens: 2000000,
   selector: "body",
