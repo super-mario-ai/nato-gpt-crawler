9b6c1148b4a2336d1fef0e6d10d22124ad0ff41d
super-mario-ai <kazamario@gmail.com>
2024-12-16 09:23:27 +0000

    *

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "https://www.gov.pl/",
   match: "https://www.gov.pl/**",
-  maxPagesToCrawl: 99999999,
+  maxPagesToCrawl: 9999999,
   outputFileName: "gov-pl.json",
   maxTokens: 2000000,
   selector: "body",
