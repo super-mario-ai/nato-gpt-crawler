7ba914de290694378164244cc42f9307b9c6d1cd
super-mario-ai <kazamario@gmail.com>
2024-12-16 09:45:17 +0000

    *

--- config.ts
+++ config.ts
@@ -1,8 +1,8 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "",
-  match: "/**",
+  url: "https://sk.wikipedia.org/wiki/Ministerstvo_zdravotn%C3%ADctva_Slovenskej_republiky",
+  match: "https://sk.wikipedia.org/wiki/Ministerstvo_zdravotn%C3%ADctva_Slovenskej_republiky/**",
   maxPagesToCrawl: 99999999,
   outputFileName: "output.json",
   maxTokens: 2000000,
