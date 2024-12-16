3d4aca90ef0abc01d1c89bdee00dc57ac33a2a72
super-mario-ai <kazamario@gmail.com>
2024-12-16 08:41:06 +0000

    *

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "https://zum.lrv.lt/en/site-tree/",
   match: "https://www.zum.lrv.lt/**",
-  maxPagesToCrawl: 99999999,
+  maxPagesToCrawl: 9999999,
   outputFileName: "output.json",
   maxTokens: 2000000,
   selector: "body",
