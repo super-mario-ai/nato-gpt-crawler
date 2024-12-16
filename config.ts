915e0754b7d6739c811055ecaec2c1b1e99e4f5f
super-mario-ai <kazamario@gmail.com>
2024-12-16 05:51:47 +0000

    *

--- config.ts
+++ config.ts
@@ -1,10 +1,10 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "",
-  match: "/**",
+  url: "https://www.tm.gov.lv/en",
+  match: "https://www.tm.gov.lv/en/**",
   maxPagesToCrawl: 99999999,
-  outputFileName: "output.json",
+  outputFileName: "tm-gov-lv.json",
   maxTokens: 2000000,
   selector: "body"
 };
