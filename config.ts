4416440b304c4ba89a76c94cac6f749565cc0da1
super-mario-ai <kazamario@gmail.com>
2024-12-16 04:45:16 +0000

    *

--- config.ts
+++ config.ts
@@ -1,10 +1,10 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://mkm.ee/en",
-  match: "https://mkm.ee/en/**",
-  maxPagesToCrawl: 999999,
-  outputFileName: "mkm.json",
+  url: "https://www.hm.ee/en",
+  match: "https://www.hm.ee/en/**",
+  maxPagesToCrawl: 99999999,
+  outputFileName: "hm.json",
   maxTokens: 2000000,
   selector: "body",
 };
