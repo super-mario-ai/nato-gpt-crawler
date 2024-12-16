f6fa9dcfd91b77cf47f6987d37f861f075764c82
super-mario-ai <kazamario@gmail.com>
2024-12-16 05:29:03 +0000

    *

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "https://www.fin.ee/en/",
   match: "https://www.fin.ee/en/**",
-  maxPagesToCrawl: 99999999,
+  maxPagesToCrawl: 9999999,
   outputFileName: "fin.json",
   maxTokens: 2000000,
   selector: "body",
