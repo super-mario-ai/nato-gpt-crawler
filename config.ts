1257f1f8139daac87b96c80aaa4376a5dd8ff5ae
super-mario-ai <kazamario@gmail.com>
2024-12-16 05:37:59 +0000

    *

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "https://www.agri.ee/en",
   match: "https://www.agri.ee/en/**",
-  maxPagesToCrawl: 99999999,
+  maxPagesToCrawl: 9999999,
   outputFileName: "agri.json",
   maxTokens: 2000000,
   selector: "body",
