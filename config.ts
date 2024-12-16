184c95a98228c989d4519344e6cb37f021d46183
super-mario-ai <kazamario@gmail.com>
2024-12-16 04:52:35 +0000

    *

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://kaitseministeerium.ee",
+  url: "https://kaitseministeerium.ee/",
   match: "https://kaitseministeerium.ee/**",
   maxPagesToCrawl: 99999999,
   outputFileName: "kaitseministeerium.json",
