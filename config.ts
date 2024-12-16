07ed99406e38464616ad75e893c7d0fd2cc6b772
super-mario-ai <kazamario@gmail.com>
2024-12-16 09:45:51 +0000

    FUCK YOU BITCH DIE

--- config.ts
+++ config.ts
@@ -2,7 +2,8 @@ import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
   url: "https://sk.wikipedia.org/wiki/Ministerstvo_zdravotn%C3%ADctva_Slovenskej_republiky",
-  match: "https://sk.wikipedia.org/wiki/Ministerstvo_zdravotn%C3%ADctva_Slovenskej_republiky/**",
+  match:
+    "https://sk.wikipedia.org/wiki/Ministerstvo_zdravotn%C3%ADctva_Slovenskej_republiky/**",
   maxPagesToCrawl: 99999999,
   outputFileName: "output.json",
   maxTokens: 2000000,
